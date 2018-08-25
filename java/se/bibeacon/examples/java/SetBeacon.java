c0d6cf4519bad8c77f19988d388b1d6c74509a38
Olof Bjarnason <olof.bjarnason@gmail.com>
2018-08-25 07:59:05 +0200

    fix sonarcloud issues

--- java/se/bibeacon/examples/java/SetBeacon.java
+++ java/se/bibeacon/examples/java/SetBeacon.java
@@ -1,6 +1,5 @@
 package se.bibeacon.examples.java;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
@@ -15,16 +14,15 @@ public class SetBeacon {
          connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
          OutputStream output = connection.getOutputStream();
          output.write(("color=" + color).getBytes());
-         InputStream response = connection.getInputStream();
+         connection.getInputStream();
       } catch( Exception e ) {
-         System.err.println( e );
          return false;
       }
       return true;
    }
 
    // Usage: <systemid> <color>
-   public static void main(String args[]) {
+   public static void main(String[] args) {
       setColor(args[0], args[1]);
    }
 }
